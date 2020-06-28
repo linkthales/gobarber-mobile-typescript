b19f9a244b9d09b844ea8455a6617564429101dc
Thales Martins <linkthales@hotmail.com>
2020-06-28 19:41:43 -0300

    feat: 🎸 Gobarber mobile in Typescript

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, HeaderTitle, UserName } from './styles';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>Thales Martins</UserName>
+        </HeaderTitle>
+      </Header>
+    </Container>
+  );
+};
+
+export default Dashboard;
